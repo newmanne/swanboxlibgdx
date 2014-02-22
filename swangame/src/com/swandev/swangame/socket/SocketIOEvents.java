c94fa6bb8e01c6a38a5ad64dc5558e707125a9be
newmann <newman-n@hotmail.com>
2014-02-21 19:29:42 -0800

    Extract the socketio stuff to a library. Can probably move some of the connect screen over as a TODO:

--- swangame/src/com/swandev/swangame/socket/SocketIOEvents.java
+++ swangame/src/com/swandev/swangame/socket/SocketIOEvents.java
@@ -2,8 +2,6 @@ package com.swandev.swangame.socket;
 
 public class SocketIOEvents {
 
-	public static final String NICKNAME_SET = "nickname_set";
-
 	// pattern game
 	public static final String PLAYING_PATTERNS = "playing_patterns";
 	public static final String FINISHED_SEQUENCE = "finished_sequence";
@@ -16,12 +14,5 @@ public class SocketIOEvents {
 	public static final String ANNOUNCEMENT = "announcement";
 	public static final String INVALID_PATTERN = "invalid_pattern";
 	public static final String GAME_OVER = "game_over";
-
-	public static final String SWAN_EMIT = "swan_emit";
-	public static final String SWAN_BROADCAST = "swan_broadcast";
-
-	public static final String GET_NICKNAMES = "swan_get_nicknames";
-
-	public static final String SCREEN_SET = "screen_set";
 
 }
