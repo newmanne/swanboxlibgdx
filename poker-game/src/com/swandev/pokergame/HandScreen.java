7df947d94fcb6cdd251ba5153f9805bc5df24f35
whittysc <scott.whitty72@gmail.com>
2014-02-21 23:03:17 -0500

    Poker Client Game Code - Initial Commit
    Lots of files. Barely renders the back of two cards. Better than
    nothing!

---
+++ poker-game/src/com/swandev/pokergame/HandScreen.java
@@ -0,0 +1,110 @@
+package com.swandev.pokergame;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+
+public class HandScreen implements Screen, InputProcessor {
+	
+	private PlayerState state;
+	private PlayerStateRenderer renderer;
+	
+	private int width, height;
+
+	@Override
+	public void show() {
+		this.state = new PlayerState();
+		renderer = new PlayerStateRenderer(this.state);
+		Gdx.input.setInputProcessor(this);
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		renderer.render(delta);
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		renderer.setSize(width,  height);
+		this.width = width;
+		this.height = height;
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
