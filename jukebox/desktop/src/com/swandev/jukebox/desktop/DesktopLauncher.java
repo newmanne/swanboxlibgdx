7f09fdb38fb5aeca6683e0e256a838abbfa74309
newmann <newman-n@hotmail.com>
2014-03-12 10:44:57 -0700

    Begin the jukebox project

---
+++ jukebox/desktop/src/com/swandev/jukebox/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.swandev.jukebox.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.swandev.jukebox.Jukebox;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Jukebox(), config);
+	}
+}
