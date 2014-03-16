54e984add0ba268b21b6cb3d71fbb6400264a08d
newmann <newman-n@hotmail.com>
2014-03-16 11:07:03 -0700

    Added swanplayer

---
+++ swanplay/gwt/src/com/swandev/swanplay/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package com.swandev.swanplay.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.swandev.swanplay.SwanPlay;
+
+public class GwtLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SwanPlay();
+        }
+}
