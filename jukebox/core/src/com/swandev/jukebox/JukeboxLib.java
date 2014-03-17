00083a54686b007938b02a7f072c06dcd9e94dc1
William Dao <william@UbuntuVM.(none)>
2014-03-17 16:33:25 -0400

    added in request song list in lib

--- jukebox/core/src/com/swandev/jukebox/JukeboxLib.java
+++ jukebox/core/src/com/swandev/jukebox/JukeboxLib.java
@@ -3,6 +3,7 @@ package com.swandev.jukebox;
 public class JukeboxLib {
 
 	public static final String SEND_SONGLIST = "send_songlist";
+	public static final String REQUEST_SONGLIST = "request_songlist";
 	public static final String ADD_TO_PLAYLIST = "add_to_playlist";
 	public static final String USER_PLAY = "user_play";
 	public static final String USER_PAUSE = "user_pause";
