2267fcb3659cdcd3896bd16ca97b252be1ac3f4a
William Dao <william@UbuntuVM.(none)>
2014-03-17 14:38:16 -0400

    adding in server sockets events

--- jukebox/core/src/com/swandev/jukebox/JukeboxLib.java
+++ jukebox/core/src/com/swandev/jukebox/JukeboxLib.java
@@ -2,6 +2,7 @@ package com.swandev.jukebox;
 
 public class JukeboxLib {
 	
+	public static final String REQUEST_SONGLIST = "request_songlist";
 	public static final String SEND_SONGLIST = "send_songlist";
 	public static final String ADD_TO_PLAYLIST = "add_to_playlist";
 	public static final String USER_PLAY = "user_play";
