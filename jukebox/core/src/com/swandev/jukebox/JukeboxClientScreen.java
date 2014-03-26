35cc0771c90138824f7bc3b7e056ddf04113b81b
newmann <newman-n@hotmail.com>
2014-03-26 10:56:37 -0700

    Send the entire song data rather than just the song via json (except for the music). Fix the yourSongIsPlaying() logic

--- jukebox/core/src/com/swandev/jukebox/JukeboxClientScreen.java
+++ jukebox/core/src/com/swandev/jukebox/JukeboxClientScreen.java
@@ -2,9 +2,11 @@ package com.swandev.jukebox;
 
 import io.socket.IOAcknowledge;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
@@ -24,8 +26,10 @@ import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.google.common.collect.Lists;
+import com.swandev.jukebox.Jukebox.SongData;
 import com.swandev.swanlib.screen.SwanGameStartScreen;
 import com.swandev.swanlib.socket.EventCallback;
 import com.swandev.swanlib.socket.SocketIOState;
@@ -38,10 +42,9 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 	private final JukeboxClient game;
 	private ImageButton playPause;
 	private ImageButton next;
-	private String currentSong;
 	private final Label currentSongInfo;
 
-	private final int fontSize = 40;
+	private final int fontSize = 20;
 	private final Table table;
 
 	private final float VIRTUAL_WIDTH = 600;
@@ -53,9 +56,14 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 
 	private final Group songGroup;
 
-	private List<String> songs;
 	public Skin skin;
 	private Label yourSelectionInfo;
+
+	private static final Json json = new Json();
+
+	private Collection<SongData> songs;
+
+	private boolean yourSongIsPlaying = false;
 
 	public JukeboxClientScreen(SocketIOState socketIO, JukeboxClient game) {
 		super(socketIO);
@@ -71,11 +79,11 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 		Label nameLabel = new Label("Swanbox Jukebox:", skin);
 		Label currentSongLabel = new Label("Current Song: ", skin);
 		Label yourSelectionLabel = new Label("Your selection: ", skin);
-		yourSelectionInfo = new Label("Example song I selected", skin);
+		yourSelectionInfo = new Label("", skin);
 		table.add(nameLabel).colspan(2);
 		table.row();
 
-		currentSongInfo = new Label(currentSong, skin);
+		currentSongInfo = new Label("", skin);
 		table.add(currentSongLabel);
 		table.add(currentSongInfo).left();
 		table.row();
@@ -84,7 +92,7 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 		table.row();
 		table.add(songGroup).fill().expand().colspan(2);
 		table.row();
-		addHostButtons(table);
+		addButtons(table);
 		buildBackground(skin);
 		stage.addActor(table);
 
@@ -100,8 +108,7 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 		stage.addActor(backgroundImage);
 	}
 
-	private void addHostButtons(Table table) {
-		Gdx.app.log("JUKEBOX", "Adding buttons for host");
+	private void addButtons(Table table) {
 		final Skin skin = game.getAssets().getSkin();
 		next = new EventSendingTextButton("SKIP", skin, JukeboxLib.USER_NEXT);
 		playPause = new PlayPauseButton(getSocketIO());
@@ -141,21 +148,34 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 		stage.dispose();
 	}
 
+	private boolean isMySongPlaying() {
+		return yourSongIsPlaying;
+	}
+
 	@Override
 	protected void registerEvents() {
 		registerEvent(JukeboxLib.SONG_OVER, new EventCallback() {
 			@Override
 			public void onEvent(IOAcknowledge ack, Object... args) {
 				songSelected = false;
+				yourSongIsPlaying = false;
+				yourSelectionInfo.setText("");
 				currentSongInfo.setText("");
 			}
+
 		});
 		registerEvent(JukeboxLib.SEND_SONGLIST, new EventCallback() {
 			@Override
 			public void onEvent(IOAcknowledge arg0, Object... args) {
 				Gdx.app.log("JUKEBOX", "song list receieved!");
-				songs = SwanUtil.parseJsonList((JSONArray) args[0]);
-				buildSongList();
+				// This code is messy and exemplifies the problem that we are using 3 different json processing engines in the same project. But whatever, that's not going to change now.
+				songs = Lists.newArrayList();
+				Gdx.app.log("HELP", JSONObject.valueToString(args));
+				JSONArray jsonArray = (JSONArray) args[0];
+				for (int i = 0; i < jsonArray.length(); i++) {
+					songs.add(json.fromJson(SongData.class, jsonArray.getJSONObject(i).toString()));
+				}
+				buildSongList(songs);
 			}
 		});
 
@@ -163,45 +183,53 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 			@Override
 			public void onEvent(IOAcknowledge ack, Object... args) {
 				currentSongInfo.setText((CharSequence) args[0]);
+				final String requester = (String) args[1];
+				if (requester.equals(getSocketIO().getNickname())) {
+					yourSongIsPlaying = true;
+				}
 			}
 		});
 
 	}
 
-	private void buildSongList() {
+	private void buildSongList(Collection<SongData> songs) {
+		songGroup.clear();
 		final Table songTable = new Table();
 		final ScrollPane scroller = new ScrollPane(songTable);
 
-		for (String songName : songs) {
-			final Group group = new Group();
-			Table songInfo = new Table();
+		for (SongData song : songs) {
+			songTable.add(new SongInfoTable(song));
+			songTable.row().height(fontSize * 2f);
+		}
+		songTable.top();
+		scroller.setFillParent(true);
+		songGroup.addActor(scroller);
+	}
 
-			Label nameLabel = new Label(songName, skin);
-			nameLabel.setName("songName");
-			songInfo.add(nameLabel).expandX().left().padLeft(10);
-			songInfo.setFillParent(true);
+	public class SongInfoTable extends Table {
 
-			fontActors.add(nameLabel);
+		// hacky way to record the "key" to use
+		final String songName;
 
-			group.addActor(songInfo);
-			group.addListener(new ClickListener() {
+		public SongInfoTable(SongData songData) {
+			super();
+			songName = songData.toString();
+			defaults().pad(10).left();
+			add(new Label(songData.getSongName(), skin));
+			add(new Label("(" + songData.getArtist() + ")", skin));
+			addListener(new ClickListener() {
 
 				@Override
 				public void clicked(InputEvent event, float x, float y) {
-					if (yourSongIsPlaying()) {
+					if (isMySongPlaying()) {
 						new Dialog("Can't select this song now", skin, "dialog").text("Please wait until your song finishes playing").button("OK").show(stage);
 					} else {
-						Label sn = (Label) group.findActor("songName");
-						selectSong(sn.getText().toString());
+						selectSong(songName);
 					}
 				}
 
-				// TODO: this won't work if you picked the song currently playing...
-				private boolean yourSongIsPlaying() {
-					return yourSelectionInfo.getText().equals(currentSongInfo.getText());
-				}
-
 				private void selectSong(final String songName) {
+					final String dialogText = songSelected ? "Switch request to " : "Request ";
 					new Dialog("Select this song?", skin, "dialog") {
 						@Override
 						protected void result(Object result) {
@@ -212,18 +240,10 @@ public class JukeboxClientScreen extends SwanGameStartScreen {
 								yourSelectionInfo.setText(songName);
 							}
 						}
-					}.text("Play " + songName + "?").button("Yes", true).button("No", false).key(Keys.ENTER, true).key(Keys.ESCAPE, false).show(stage);
+					}.text(dialogText + songName + "?").button("Yes", true).button("No", false).key(Keys.ENTER, true).key(Keys.ESCAPE, false).show(stage);
 				}
 			});
-
-			songTable.row().height(fontSize * 3);
-			songTable.add(group).expandX().left();
-
 		}
-		songTable.top();
-		scroller.setFillParent(true);
-		songGroup.addActor(scroller);
-
 	}
 
 	@Override
