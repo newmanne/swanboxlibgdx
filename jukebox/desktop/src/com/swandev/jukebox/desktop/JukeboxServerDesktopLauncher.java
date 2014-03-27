b1344540fc91190348a0b368415d6910c99dfada
newmann <newman-n@hotmail.com>
2014-03-27 12:11:56 -0700

    some client screen updates to record orientation

--- jukebox/desktop/src/com/swandev/jukebox/desktop/JukeboxServerDesktopLauncher.java
+++ jukebox/desktop/src/com/swandev/jukebox/desktop/JukeboxServerDesktopLauncher.java
@@ -7,6 +7,7 @@ import com.swandev.jukebox.JukeboxServer;
 public class JukeboxServerDesktopLauncher {
 	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		// config.fullscreen = true;
 		new LwjglApplication(new JukeboxServer(), config);
 	}
 }
